45c2ee88cd2b14885010ea156189428e6c9fb909
Samuel Magafu <hi@samuelmagafu.com>
2024-09-02 23:36:48 +0300

    fixed all codes

--- community/admin.py
+++ community/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from . models import Category, Thread,   ThreadReply, ThreadLike, PostLike
 
 # Register your models here.
+admin.site.register(Category)
 admin.site.register(Thread)
 admin.site.register(ThreadReply)
