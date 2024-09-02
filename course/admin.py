4cae99085062f5cfb569e42f6f6f2545a9c3cfe3
Samuel Magafu <hi@samuelmagafu.com>
2024-09-02 23:32:35 +0300

    fixed admin panel

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Course,Module,Video,Document,Question,Quiz,Answer,Enrollment
 # Register your models here.
+admin.site.register(Course)
+admin.site.register(Module)
+admin.site.register(Video)
+admin.site.register(Document)
+admin.site.register(Question)
+admin.site.register(Quiz)
+admin.site.register(Answer)
+admin.site.register(Enrollment)
