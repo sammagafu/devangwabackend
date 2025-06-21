59b39bb8bf33b74c4197049165cdadf7a6fa1677
Samuel Magafu <hi@samuelmagafu.com>
2025-06-21 14:05:03 +0300

    updated the setting

--- devangwa/settings.py
+++ devangwa/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=m6yb)rmzyf%_oe55n78t&%%(go+dzfs8rf@z-+8_9k!y&u+ou
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['24.144.85.117 devangwacoaching.com www.devangwacoaching.com']
 
 # Application definition
 INSTALLED_APPS = [
